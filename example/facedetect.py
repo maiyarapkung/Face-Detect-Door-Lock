1860d34b290dc7b39e7a65e5916ee15627b3fe10
maiyarapkung <siratee@windowslive.com>
2018-04-23 06:54:05 +0000

    Update Face Detect.py

--- example/facedetect.py
+++ example/facedetect.py
@@ -3,35 +3,37 @@ import picamera
 import cv2
 import numpy
 
+
+a = 1
+while a == 1:
 #Create a memory stream so photos doesn't need to be saved in a file
-stream = io.BytesIO()
+	stream = io.BytesIO()
 
 #Get the picture (low resolution, so it should be quite fast)
 #Here you can also specify other parameters (e.g.:rotate the image)
-with picamera.PiCamera() as camera:
-    camera.resolution = (320, 240)
-    camera.capture(stream, format='jpeg')
+
+
+	with picamera.PiCamera() as camera:
+    		camera.resolution = (320, 240)
+    		camera.capture(stream, format='jpeg')
 
 #Convert the picture into a numpy array
-buff = numpy.fromstring(stream.getvalue(), dtype=numpy.uint8)
+	buff = numpy.fromstring(stream.getvalue(), dtype=numpy.uint8)
 
 #Now creates an OpenCV image
-image = cv2.imdecode(buff, 1)
+	image = cv2.imdecode(buff, 1)
 
 #Load a cascade file for detecting faces
-face_cascade = cv2.CascadeClassifier('/home/pi/opencv-3.4.1/haarcascades/haarcascade_frontalface_alt.xml')
+	face_cascade = cv2.CascadeClassifier('/home/pi/opencv-3.4.1/data/haarcascades/haarcascade_frontalface_alt.xml')
 
 #Convert to grayscale
-gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+	gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
 #Look for faces in the image using the loaded cascade file
-faces = face_cascade.detectMultiScale(gray, 1.5, 5)
+	faces = face_cascade.detectMultiScale(gray, 1.5, 3)
 
-print "Found "+str(len(faces))+" face(s)"
+	print "Found "+str(len(faces))+" face(s)"
 
-#Draw a rectangle around every found face
-for (x,y,w,h) in faces:
-    cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
-
-#Save the result image
-cv2.imwrite('result.jpg',image)
+	if len(faces) == 1:
+		a = 2
+		cv2.imwrite('detectedface.jpeg',image)
